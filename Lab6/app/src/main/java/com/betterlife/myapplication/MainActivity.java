4e75fd2bac0916e577c92d709d6645f4a2d2e15a
Mankirat <kaur0474@algonquinlive.com>
2020-11-13 17:01:01 +0530

    done all the work

---
+++ Lab6/app/src/main/java/com/betterlife/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.betterlife.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void openweatherforecast(View view) {
+
+        startActivity(new Intent(getApplicationContext(), WeatherForecast.class));
+
+    }
+}
